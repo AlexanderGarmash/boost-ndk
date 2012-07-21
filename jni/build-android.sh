af436d91d7fcc8a5cc9764b2a7a43c98c1da2826
mcxiaoke <mcxiaoke@gmail.com>
2012-07-21 11:59:59 +0800

    modify build-android.bat script

--- jni/build-android.sh
+++ jni/build-android.sh
@@ -1,1 +1,1 @@
-./b2 --without-python --without-mpi --without-wave toolset=gcc-android4.4.3 link=static runtime-link=static target-os=linux --stagedir=android --layout=system
+./b2 --without-python --without-mpi --without-wave --without-graph_parallel --without-graph --without-locale toolset=gcc-android4.4.3 link=static runtime-link=static target-os=linux --stagedir=android --layout=system
